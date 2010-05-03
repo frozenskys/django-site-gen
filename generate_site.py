b85614b88f96e6b36909e6ac6e9e627d6d78870f
Charles Leifer <coleifer@gmail.com>
2010-05-03 18:42:13 -0500

    Moving more configuration into the conf.yaml file

--- generate_site.py
+++ generate_site.py
@@ -22,6 +22,11 @@ settings.configure(
 
 fh = open('conf.yaml', 'r')
 config = yaml.load(fh)
+
+file_mapping = config['files']
+local_file_mapping = config['local_files']
+directories = config['directories']
+
 config = config['settings']
 fh.close()
 
@@ -30,7 +35,14 @@ config.update({
     'site_name': site_name,
     'port': port,
     'local': False,
-    'secret_key': secret_key
+    'secret_key': secret_key,
+})
+config.update({
+    'site_root': directories['site_root'] % config,
+    'wsgi': directories['wsgi'] % config,
+    'media': directories['media'] % config,
+    'templates': directories['templates'] % config,
+    'logs': directories['logs'] % config
 })
 
 context = Context(config)
@@ -45,52 +57,24 @@ def write_template(template_name, dest, context):
 
 # create the directory structure for the site
 try:
-    os.makedirs('%(base_site)s%(site_name)s.com/%(site_name)s/apache/' % context)
-    os.makedirs('%(base_site)s%(site_name)s.com/%(site_name)s/media/' % context)
-    os.makedirs('%(base_site)s%(site_name)s.com/%(site_name)s/templates/' % context)
-    os.makedirs('%(base_site)s%(site_name)s.com/logs/' % context)
-    os.makedirs('%(base_site)s%(site_name)s.com/private/' % context)
+    for directory in directories.values():
+        os.makedirs(directory % context)
 except OSError:
     pass
 
-# load mapping of template -> destination file
-mapping = {
-    'settings.py.conf': '%(base_site)s%(site_name)s.com/%(site_name)s/settings.py',
-    'urls.py.conf': '%(base_site)s%(site_name)s.com/%(site_name)s/urls.py',     
-    'manage.py.conf': '%(base_site)s%(site_name)s.com/%(site_name)s/manage.py',
-    'empty.conf': '%(base_site)s%(site_name)s.com/%(site_name)s/__init__.py',
-    'empty.conf': '%(base_site)s%(site_name)s.com/logs/apache_error.log',
-    'empty.conf': '%(base_site)s%(site_name)s.com/logs/apache_access.log',
-    'empty.conf': '%(base_site)s%(site_name)s.com/logs/nginx_error.log',
-    'empty.conf': '%(base_site)s%(site_name)s.com/logs/nginx_access.log',
-    'wsgi.conf': '%(base_site)s%(site_name)s.com/%(site_name)s/apache/%(site_name)s.wsgi',
-    'apache.conf': '%(base_site)s%(site_name)s.com/apache_%(site_name)s.com',
-    'nginx.conf': '%(base_site)s%(site_name)s.com/nginx_%(site_name)s.com',
-    'hosts.conf': '%(base_site)s%(site_name)s.com/hosts',
-    'ports.conf': '%(base_site)s%(site_name)s.com/ports.conf'
-}
-
-# mapping of template -> destination file for local dev settings
-local_mapping = {
-    'apache.conf': '%(base_site)s%(site_name)s.com/local_apache_%(site_name)s.com',
-    'nginx.conf': '%(base_site)s%(site_name)s.com/local_nginx_%(site_name)s.com',
-    'hosts.conf': '%(base_site)s%(site_name)s.com/local_hosts',
-}
-
 # write all files
-for template_name, destination in mapping.items():
+for destination, template_name in file_mapping.items():
     write_template(template_name, destination, context)
 
 # write local versions of the config files
 context['local'] = True
-for template_name, destination in local_mapping.items():
+for destination, template_name in local_file_mapping.items():
     write_template(template_name, destination, context)
 
 print 'Creating database %(site_name)s_main' % context
 os.system('createdb %(site_name)s_main' % context)
-os.system('chmod 755 %(base_site)s%(site_name)s.com/%(site_name)s/manage.py' % (context))
 
 print 'Copying media directory'
-os.system('cp -R ./media/* %(base_site)s%(site_name)s.com/%(site_name)s/media/' % (context))
+os.system('cp -R ./media/* %(media)s' % (context))
 
 print 'Done!'
